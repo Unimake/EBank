9cc0ba28447e5d17703e766168c86a0db6f33e16
Marcelo de Souza <marcelo-unimake@users.noreply.github.com>
2022-05-24 08:27:00 -0300

    Exemplo em VB.net #146353

---
+++ examples/VBNet/Unimake.EBank.Solutions.RegisterBillet/Unimake.EBank.Solutions.RegisterBillet/MessageCenter.vb
@@ -0,0 +1,89 @@
+﻿Imports System.Console
+
+Public Class MessageCenter
+
+#Region "Private Constructors"
+
+    Private Sub New()
+    End Sub
+
+#End Region
+
+#Region "Private Methods"
+
+    Private Shared Sub WriteLine(ByVal line As String)
+        Dim size = 81 - line.Length
+        Console.WriteLine($"| {line}{"".PadLeft(size)}|")
+    End Sub
+
+    Private Shared Sub WriteTitle(ByVal title As String)
+        WriteLine($"        {title}")
+    End Sub
+
+#End Region
+
+#Region "Internal Methods"
+
+    Friend Shared Sub Log(ByVal pLog As String)
+        Dim oldColor = ForegroundColor
+        ForegroundColor = ConsoleColor.Blue
+        Console.WriteLine(pLog)
+        ForegroundColor = oldColor
+    End Sub
+
+#End Region
+
+#Region "Public Methods"
+
+    Public Shared Sub [Error](ByVal ex As Exception)
+        Dim oldColor = ForegroundColor
+        ForegroundColor = ConsoleColor.DarkRed
+        Call DrawLine()
+        WriteTitle("ERROR!!!")
+        ForegroundColor = ConsoleColor.Red
+        Dim message = ex.GetAllMessages()
+
+        For Each item In Split(message, 80)
+            WriteLine(item)
+        Next
+
+        Call DrawLine()
+        Call NewLine()
+
+        ForegroundColor = oldColor
+    End Sub
+
+    Public Shared Sub Alert(ByVal message As String, ByVal Optional title As String = "Aviso!")
+        Dim oldColor = ForegroundColor
+        ForegroundColor = ConsoleColor.DarkYellow
+
+        Call DrawLine()
+        WriteTitle(title)
+
+        ForegroundColor = ConsoleColor.Yellow
+
+        For Each item In Split(message, 80)
+            WriteLine(item)
+        Next
+
+        Call DrawLine()
+        Call NewLine()
+
+        ForegroundColor = oldColor
+    End Sub
+
+    Public Shared Sub DrawLine()
+        Console.WriteLine("_".PadLeft(84, "_"c))
+    End Sub
+
+    Public Shared Sub NewLine()
+        Console.WriteLine()
+    End Sub
+
+    Public Shared Function Split(ByVal str As String, ByVal chunkSize As Integer) As IEnumerable(Of String)
+        Return If(str.Length <= chunkSize, (New String() {str}), Enumerable.Range(0, str.Length / chunkSize).Select(Function(i) str.Substring(i * chunkSize, chunkSize)))
+    End Function
+
+#End Region
+
+End Class
---
+++ examples/VBNet/Unimake.EBank.Solutions.RegisterBillet/Unimake.EBank.Solutions.RegisterBillet/Program.vb
@@ -0,0 +1,86 @@
+Imports EBank.Solutions.Primitives.Billet.Models
+Imports EBank.Solutions.Primitives.Enumerations
+Imports EBank.Solutions.Primitives.Enumerations.Billet
+Imports EBank.Solutions.Primitives.Exceptions.Response.Billet
+Imports Unimake.AuthServer.Authentication
+Imports Unimake.EBank.Solutions.Services.Billet
+Imports Unimake.EBank.Solutions.Services.Billet.Request
+
+Friend Module Program
+
+    Public Sub DumpAsJson(value As Object)
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(value))
+    End Sub
+
+    Public Sub Main()
+        Console.Title = "Registrar boleto"
+        MessageCenter.Log("Iniciando registros de boletos")
+        Dim queryService = New BilletService()
+        Dim took = New TimeSpan()
+
+        ' Boleto mínimo para gravação
+        ' CPF e CNPJ foram gerados no site https://www.4devs.com.br
+
+        Dim request = New RegisterRequest With
+        {
+            .Especie = EspecieTitulo.Outros,
+            .ValorNominal = 45.88D,
+            .Vencimento = Date.Today.AddDays(15),
+            .NumeroNaEmpresa = "12345",
+            .NumeroNoBanco = "12345",
+            .Beneficiario = New Beneficiario With
+            {
+                .Codigo = "1234",
+                .Nome = "Unimake Software",
+                .Inscricao = "71444314000121",
+                .Conta = New ContaCorrente With
+                {
+                    .Banco = Banco.BancoDoBrasil,
+                    .Agencia = "0246",
+                    .Numero = "0246"
+                }
+            },
+            .Pagador = New Pagador With
+            {
+                .Nome = "Marcelo de Souza",
+                .Email = "pagador@exemplo.com.br",
+                .TipoInscricao = TipoInscricao.CPF,
+                .Inscricao = "38640211035",
+                .Endereco = New Endereco With
+                {
+                    .Rua = "Rua Fictícia",
+                    .Numero = "11",
+                    .Bairro = "Bairro",
+                    .CEP = "11111111",
+                    .Cidade = "Brasília",
+                    .UF = "DF"
+                }
+            }
+        }
+
+        Try
+            ' Para utilização do e-bank, é necessário utilizar-se de um escopo autenticado.
+            ' Logo, iremos criar o nosso escopo
+            ' Talvez você não consiga realizar os testes de emissão de seus boletos com estas informações.
+            ' Para que seu boleto seja válido, deverá entrar em contato com a Unimake Software em http://www.unimake.com.br/ e obter suas chaves.
+            ' Este AppId e Secret foram criados apenas para testes.
+            Dim scope = New Services.Security.AuthenticationService().AuthenticateAsync(New AuthenticationRequest With
+            {
+                .AppId = "61a73f4735ad4993959e28e2b0e4552a",
+                .Secret = "35955532e0c54517bc9d7e900b61b8d3"
+            }).GetAwaiter().GetResult()
+            Dim service = New BilletService()
+            Dim response = service.RegisterAsync(request, scope).GetAwaiter().GetResult()
+            DumpAsJson(response)
+        Catch registerEx As RegisterResponseException
+            DumpAsJson(registerEx.Errors)
+            MessageCenter.Error(registerEx)
+        End Try
+
+        MessageCenter.DrawLine()
+        MessageCenter.Alert("Registro concluído.")
+        Dim unused = Console.ReadLine()
+
+    End Sub
+
+End Module

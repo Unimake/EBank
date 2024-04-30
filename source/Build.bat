f6fe52b51261db441acbf65f7d2adb6cbc7b0d31
Marcelo de Souza <marcelo-unimake@users.noreply.github.com>
2024-04-30 17:01:55 -0300

    Removido assinatura buidl.bat. #163181

--- source/Build.bat
+++ source/Build.bat
@@ -4,10 +4,10 @@ SET destDir=%2%
 @ECHO Definindo o diretório %destDir% para uso
 @cls
 ECHO Assinando arquivo %source%
-SET signtool="C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe"
-SET signtoolParams=sign /n "UNIMAKE"
-CALL %signtool% %signtoolParams% %source%
-ECHO Sign ERRORLEVEL = %errorlevel%
+:: SET signtool="C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe"
+:: SET signtoolParams=sign /n "UNIMAKE"
+:: CALL %signtool% %signtoolParams% %source%
+:: ECHO Sign ERRORLEVEL = %errorlevel%
 
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe %source% /codebase /tlb
 ECHO Register tlb ERRORLEVEL = %errorlevel%

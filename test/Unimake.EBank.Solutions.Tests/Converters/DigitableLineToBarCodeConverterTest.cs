e5518e6926970f8ac21d91da7fb89c59a81cdb41
Marcelo <6012333+marcelo-unimake@users.noreply.github.com>
2024-09-19 04:08:33 -0300

    #166651
    Converter a linha digitável de 47 para código de barras com 44 posições

---
+++ test/Unimake.EBank.Solutions.Tests/Converters/DigitableLineToBarCodeConverterTest.cs
@@ -0,0 +1,37 @@
+﻿using Unimake.EBank.Solutions.Converters;
+using Unimake.EBank.Solutions.Tests.Abstractions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Unimake.EBank.Solutions.Tests.Converters
+{
+    public class DigitableLineToBarCodeConverterTest : TestBase
+    {
+        #region Public Constructors
+
+        public DigitableLineToBarCodeConverterTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        // Este teste foi realizado com boletos reais, foram removidos para não infringir a LGPD
+        // [Theory]
+        // [InlineData("", "")]
+        // [InlineData("", "")]
+        // [InlineData("", "")]
+        // [InlineData("", "")]
+        // [InlineData("", "")]
+        // [InlineData("", "")]
+        // [InlineData("", "")]
+        // public void Convert(string digitableLine, string expectedBarCode)
+        // {
+        //     var codeBar = DigitableLineToBarCodeConverter.ToBarCode(digitableLine);
+        //     Assert.Equal(expectedBarCode, codeBar);
+        // }
+
+        #endregion Public Methods
+    }
+}

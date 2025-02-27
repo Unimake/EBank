d4baad856a26645c7b856c1e579d78340c5daeb3
Marcelo <6012333+marcelo-unimake@users.noreply.github.com>
2025-02-27 18:41:28 -0300

    feat: Atualizado exemplo GIT em VB.net para emissão de boletos. ID #171073

--- examples/VBNet/Unimake.EBank.Solutions.RegisterBillet/Unimake.EBank.Solutions.RegisterBillet/MessageCenter.vb
+++ examples/VBNet/Unimake.EBank.Solutions.RegisterBillet/Unimake.EBank.Solutions.RegisterBillet/MessageCenter.vb
@@ -71,16 +71,38 @@ Public Class MessageCenter
 
         ForegroundColor = oldColor
     End Sub
-
     Public Shared Sub DrawLine()
         Console.WriteLine("_".PadLeft(84, "_"c))
     End Sub
+    Public Shared Sub Message(ByVal message As String)
+        Call DrawLine()
 
+        For Each item In Split(message, 80)
+            WriteLine(item)
+        Next
+
+        Call DrawLine()
+        Call NewLine()
+    End Sub
+
+    Public Shared Sub Message(ByVal messages As IEnumerable(Of String))
+        Call DrawLine()
+
+        For Each msg In messages
+            For Each item In Split(msg, 80)
+                WriteLine(item)
+            Next
+        Next
+
+        Call DrawLine()
+        Call NewLine()
+    End Sub
     Public Shared Sub NewLine()
         Console.WriteLine()
     End Sub
 
-    Public Shared Function Split(ByVal str As String, ByVal chunkSize As Integer) As IEnumerable(Of String)
+    Public Shared Function Split(str As String, chunkSize As Integer) As IEnumerable(Of String)
+        str = str.Replace(Environment.NewLine, " ")
         Return If(str.Length <= chunkSize, (New String() {str}), Enumerable.Range(0, str.Length / chunkSize).Select(Function(i) str.Substring(i * chunkSize, chunkSize)))
     End Function
 
--- examples/VBNet/Unimake.EBank.Solutions.RegisterBillet/Unimake.EBank.Solutions.RegisterBillet/Program.vb
+++ examples/VBNet/Unimake.EBank.Solutions.RegisterBillet/Unimake.EBank.Solutions.RegisterBillet/Program.vb
@@ -1,11 +1,14 @@
+Imports System.IO
+Imports EBank.Solutions.Primitives.Billet
 Imports EBank.Solutions.Primitives.Billet.Models
 Imports EBank.Solutions.Primitives.Enumerations
 Imports EBank.Solutions.Primitives.Enumerations.Billet
 Imports EBank.Solutions.Primitives.Exceptions.Response.Billet
-Imports Unimake.AuthServer.Authentication
+Imports EBank.Solutions.Primitives.PDF.Models
 Imports Unimake.AuthServer.Security.Scope
 Imports Unimake.EBank.Solutions.Services.Billet
 Imports Unimake.EBank.Solutions.Services.Billet.Request
+Imports Unimake.Primitives.Security.Credentials
 
 Friend Module Program
 
@@ -17,30 +20,37 @@ Friend Module Program
         Console.Title = "Registrar boleto"
         MessageCenter.Log("Iniciando registros de boletos")
         Dim billetService = New BilletService()
+        Dim exitCode = 1
 
         ' Boleto mínimo para gravação
         ' CPF e CNPJ foram gerados no site https://www.4devs.com.br
 
+        ' Este configuration id, informado aqui, é o
+        ' identificador da conta corrente que será utilizada na emissão do boleto
+
+        ' Com o configurationId, a propriedade Beneficiário não precisa mais ser informada
+        '.Beneficiario = New Beneficiario With
+        '{
+        '    .Codigo = "1234",
+        '    .Nome = "Unimake Software",
+        '    .Inscricao = "71444314000121",
+        '    .Conta = New ContaCorrente With
+        '    {
+        '        .Banco = Banco.Itau,
+        '        .Agencia = "0246",
+        '        .Numero = "0246"
+        '    }
+        '},
+
         Dim request = New RegisterRequest With
         {
+            .ConfigurationId = "ZCKWGQ55LTDXKYYC",
             .Testing = True,
             .Especie = EspecieTitulo.Outros,
             .ValorNominal = 45.88D,
             .Vencimento = Date.Today.AddDays(15),
             .NumeroNaEmpresa = "12345",
             .NumeroNoBanco = "12345",
-            .Beneficiario = New Beneficiario With
-            {
-                .Codigo = "1234",
-                .Nome = "Unimake Software",
-                .Inscricao = "71444314000121",
-                .Conta = New ContaCorrente With
-                {
-                    .Banco = Banco.Itau,
-                    .Agencia = "0246",
-                    .Numero = "0246"
-                }
-            },
             .Pagador = New Pagador With
             {
                 .Nome = "Marcelo de Souza",
@@ -49,7 +59,7 @@ Friend Module Program
                 .Inscricao = "38640211035",
                 .Endereco = New Endereco With
                 {
-                    .Rua = "Rua Fictícia",
+                    .Logradouro = "Rua Fictícia",
                     .Numero = "11",
                     .Bairro = "Bairro",
                     .CEP = "11111111",
@@ -59,28 +69,71 @@ Friend Module Program
             }
         }
 
+        ' Aqui, vamos solicitar o PDF
+        request.PDFConfig = New PDFConfig With
+        {
+            .TryGeneratePDF = True
+        }
+
+        ' E se permitir boleto híbrido, vamos solicitar o bolet híbrido
+        request.PIXConfig = New PIXBilletConfig With
+        {
+            .RegistrarPIX = True,
+            .Chave = "chave@example.com"
+        }
+
         Try
             ' Para utilização do e-bank, é necessário utilizar-se de um escopo autenticado.
             ' Logo, iremos criar o nosso escopo
             ' Talvez você não consiga realizar os testes de emissão de seus boletos com estas informações.
             ' Para que seu boleto seja válido, deverá entrar em contato com a Unimake Software em http://www.unimake.com.br/ e obter suas chaves.
             ' Este AppId e Secret foram criados apenas para testes.
-            Dim scope = New AuthenticatedScope(New AuthenticationRequest With
+            Dim scope = New AuthenticatedScope(New AuthenticationToken With
             {
-                .AppId = "61a73f4735ad4993959e28e2b0e4552a",
-                .Secret = "35955532e0c54517bc9d7e900b61b8d3"
+                .AppId = "e7be62b0e70a4488aa2b62ddc2a9738a",
+                .Secret = "e3f50cb68e8a49f48dbf2a26b3db0383"
             })
+
+            ' Criamos o serviço para emissão de boletos
             Dim service = New BilletService()
+
+            ' Enviamos a solicitação de registro
             Dim response = service.RegisterAsync(request, scope).GetAwaiter().GetResult()
-            DumpAsJson(response)
+
+            ' Como não deu nenhum erro, tratados abaixo, o boleto foi registrado.
+
+            MessageCenter.DrawLine()
+            Dim messages = New List(Of String) From {
+                "Número no Banco  : " + response.NumeroNoBanco,
+                "Codigo de Barras : " + response.CodigoBarraNumerico,
+                "Linha Digitável  : " + response.LinhaDigitavel
+            }
+
+            MessageCenter.Message(messages)
+
+            ' Vamos exibir o pdf. no visualizador padrão
+            ' O conteúdo do .pdf é base64
+
+            File.WriteAllBytes("ebank-boleto.pdf", Convert.FromBase64String(response.PDFContent.Content))
+            Process.Start(New ProcessStartInfo With
+                         {
+                             .FileName = "ebank-boleto.pdf",
+                             .UseShellExecute = True
+                         })
+
+            exitCode = 0
         Catch registerEx As RegisterResponseException
-            DumpAsJson(registerEx.Errors)
+            DumpAsJson(registerEx)
             MessageCenter.Error(registerEx)
+        Catch ex As Exception
+            DumpAsJson(ex)
+            MessageCenter.Error(ex)
         End Try
 
         MessageCenter.DrawLine()
         MessageCenter.Alert("Registro concluído.")
         Dim unused = Console.ReadLine()
+        Environment.Exit(exitCode)
 
     End Sub
 
